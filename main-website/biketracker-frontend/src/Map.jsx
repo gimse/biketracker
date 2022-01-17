c3c4f5a80e24c1d9593c6ad7f1d04cd9ffed0985
gimse <23360355+gimse@users.noreply.github.com>
2022-01-17 18:49:46 +0100

    working maxboundary

--- main-website/biketracker-frontend/src/Map.jsx
+++ main-website/biketracker-frontend/src/Map.jsx
@@ -3,12 +3,19 @@ import VectorTileLayer from 'react-leaflet-vector-tile-layer';
 //import "leaflet/dist/leaflet.css";
 import './Map.css';
 
+
+const outerBounds = [
+  [59.81877,10.470149],
+  [60.02576,10.945497],
+]
+
 function App() {
+
+
   return (
-    <MapContainer center={[59.9274,10.707]} zoom={12}scrollWheelZoom={false}>
+    <MapContainer center={[59.9274,10.707]} maxBounds={outerBounds} zoom={11} minZoom={11} maxZoom={14} scrollWheelZoom={true}>
       <VectorTileLayer
       styleUrl="/styles/basic-preview/style.json"
-      //url="/data/v3/{z}/{x}/{y}.pbf"
         
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
