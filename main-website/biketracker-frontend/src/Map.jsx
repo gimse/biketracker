f4c88248c6826331c382790e10b215f1ad4fa1cc
gimse <23360355+gimse@users.noreply.github.com>
2022-01-17 17:49:32 +0100

    working with pbf

--- main-website/biketracker-frontend/src/Map.jsx
+++ main-website/biketracker-frontend/src/Map.jsx
@@ -1,12 +1,15 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import VectorTileLayer from 'react-leaflet-vector-tile-layer';
 //import "leaflet/dist/leaflet.css";
 import './Map.css';
 
 function App() {
   return (
     <MapContainer center={[59.9274,10.707]} zoom={12}scrollWheelZoom={false}>
-      <TileLayer
-    url="/styles/basic-preview/{z}/{x}/{y}.png"
+      <VectorTileLayer
+      styleUrl="/styles/basic-preview/style.json"
+      //url="/data/v3/{z}/{x}/{y}.pbf"
+        
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
     </MapContainer>
